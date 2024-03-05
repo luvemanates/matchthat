3020df4d22c8a6882ff6cbbab981e140f67267d4
luvemanates <luvemanates@gmail.com>
2024-03-05 12:00:33 -0800

    client is succesfully receiving the cipher - but need to get the public key of the server and use that to share the secret back to confirm cipher received (#8)

--- lib/tasks/matchcoinbank_mintclient.rb
+++ lib/tasks/matchcoinbank_mintclient.rb
@@ -3,6 +3,29 @@ require 'json'
 require_relative 'matchthat_cryptography'
 require_relative 'digital_wallet'
 
+
+def encode64(string)
+  string = string.to_s unless string.is_a?(String)
+  Base64.encode64(string)
+end
+
+def decode64(string)
+  string = string.to_s unless string.is_a?(String)
+  Base64.decode64(string)
+end
+
+def convert_utf8(string)
+  string = string.to_s unless string.is_a?(String)
+  string.force_encoding("ISO-8859-1").encode("UTF-8")
+
+end
+
+def convert_ascii8bit(string)
+  string = string.to_s unless string.is_a?(String)
+  string.force_encoding("UTF-8").encode('ASCII-8BIT')
+end
+
+
 bank_crypto = MatchThatCryptography.new(MatchThatCryptography::CONFIG)
 bank_wallet = DigitalWallet.new('Bank Wallet', bank_crypto)
 
@@ -10,11 +33,25 @@ bank_wallet = DigitalWallet.new('Bank Wallet', bank_crypto)
 host = 'localhost'
 port = 2000
 bank_client_socket = TCPSocket.open(host, port)
-request = { 'public_key' => bank_crypto.public_key }.to_json
+request = { 'public_key' => encode64(bank_crypto.public_key) }.to_json
 bank_client_socket.puts(request)
-params = JSON.parse(bank_client_socket.gets)
+response = bank_client_socket.gets
+params = JSON.parse(response) unless response.nil?
 puts "params is "
 puts params
+#unless not params.nil?
+  puts 'in block'
+  encrypted_message = params["encrypted_message"]
+  encrypted_message = decode64(encrypted_message)
+
+  decrypted_message = bank_crypto.decrypt_message_with_private_key(encrypted_message)
+  puts "decrypted message"
+  puts "\n\n"
+  puts "decrypted mesage IS "
+  puts decrypted_message
+
+  #here is where we need to get the public key and encrypt it with the decrypted_message
+#end
 bank_client_socket.close
 
 
--- lib/tasks/matchthat_mintserver.rb
+++ lib/tasks/matchthat_mintserver.rb
@@ -17,26 +17,54 @@ mint_wallet = DigitalWallet.new('mint_wallet', matchthatmint_crypto)
 exchange = CentralizedExchange.new
 
 random_secret = (0...16).map { (65 + rand(26)).chr }.join
+puts "random secret is " + random_secret.to_s
 
+def encode64(string)
+  string = string.to_s unless string.is_a?(String)
+  Base64.encode64(string)
+end
 
+def decode64(string)
+  string = string.to_s unless string.is_a?(String)
+  Base64.decode64(string)
+end
+
+def convert_utf8(string)
+  string = string.to_s unless string.is_a?(String)
+  string.force_encoding("ISO-8859-1").encode("UTF-8")
+end
+
+def convert_ascii8bit(string)
+  string = string.to_s unless string.is_a?(String)
+#  string.force_encoding('ASCII-8BIT')  
+  string.force_encoding("ISO-8859-1").encode('ASCII-8BIT')
+end
 
 loop {
   client = server.accept
   params = JSON.parse(client.gets)
   if not params["public_key"].nil?
+    puts "the public key from the client is "
+    puts params["public_key"]
+    client_public_key = decode64( params["public_key"] )
     #other_secret = matchthat_crypto.encrypt_message(mint_wallet_crypto_card, params[:public_key], "Requesting deposit authorization:", random_secret)
-    encrypted_message = matchthatmint_crypto.encrypt_message_with_recipient_public_key(params["public_key"], random_secret)
-    decrypted_message = matchthatbank_crypto.decrypt_message_with_private_key(encrypted_message)
-    puts 'encrypted message is encoding is ' 
-    encrypted_message = "See if this is even recieved"
+    encrypted_message = matchthatmint_crypto.encrypt_message_with_recipient_public_key(client_public_key, random_secret)
+#    decrypted_message = matchthatbank_crypto.decrypt_message_with_private_key(encrypted_message)
+    puts 'encrypted message ' 
+    puts 'encoding is ' 
+    puts encrypted_message.encoding
+#    encrypted_message = "See if this is even recieved"
     #puts encrypted_message
-    client.puts({'encrypted_message' => encrypted_message}.to_json)
+    encrypted_message_utf8 = encode64(encrypted_message)
+    puts "sending message with encoding " 
+    puts encrypted_message_utf8.encoding
+    client.puts({'encrypted_message' => encrypted_message_utf8}.to_json)
+    params = JSON.parse(client.gets)
+    puts "returning from client after decrypt"
+    puts params
   end
-  puts "the public key from the client is "
   #client.puts params[:public_key ]
-  puts params["public_key"]
   #raise params.inspect
-
 }
 
 =begin
