d8e3f8c3adafa40c1d2d6f1dac3526c216a2b2a5
luvemanates <luvemanates@gmail.com>
2024-03-07 14:20:02 -0800

    updated the client code to have a secureconnection and updated run (#8)

--- lib/tasks/matchcoinbank_mintclient.rb
+++ lib/tasks/matchcoinbank_mintclient.rb
@@ -16,67 +16,81 @@ end
 
 class MintClientBank
 
+  attr_accessor :bank_client
+  attr_accessor :bank_crypto
+  attr_accessor :bank_wallet
+  attr_accessor :cipher
+  attr_accessor :cipher_key
+  attr_accessor :ciphera_iv
+
   def initialize
+    @bank_crypto = MatchThatCryptography.new(MatchThatCryptography::CONFIG)
+    @bank_wallet = DigitalWallet.new('Bank Wallet', @bank_crypto)
   end
 
   def run
-    bank_crypto = MatchThatCryptography.new(MatchThatCryptography::CONFIG)
-    bank_wallet = DigitalWallet.new('Bank Wallet', bank_crypto)
-
-
-    host = 'localhost'
-    port = 2000
-    bank_client_socket = TCPSocket.open(host, port)
-    request = { 'public_key' => encode64(bank_crypto.public_key) }.to_json
-    bank_client_socket.puts(request)
-    response = bank_client_socket.gets
-    params = JSON.parse(response) unless response.nil?
-    puts "params is "
-    puts params
-    #unless not params.nil?
-      puts 'in block'
-      encrypted_message = params["encrypted_cipher"]
-      encrypted_message = decode64(encrypted_message)
-
-      encrypted_iv = params["encrypted_cipher_iv"]
-
-      decrypted_message = bank_crypto.decrypt_message_with_private_key(encrypted_message)
-      decrypted_iv = bank_crypto.decrypt_message_with_private_key(decode64(encrypted_iv))
-
-      puts "\n\n"
-      puts "decrypted mesage IS "
-      puts decrypted_message
-
-      #here is where we need to get the public key and encrypt it with the decrypted_message
-    #end
-    response = bank_client_socket.gets
-    params = JSON.parse(response) unless response.nil?
-    puts "params is "
-    puts params
-    server_public_key = decode64(params["public_key"])
-    encrypted_secret = bank_crypto.encrypt_message_with_recipient_public_key(server_public_key, decrypted_message)
-    bank_client_socket.puts( {"encrypted_secret" => encode64(encrypted_secret) }.to_json )
-    puts "sent encrypted secret"
-
-    response = bank_client_socket.gets
+    response = @bank_client.gets
     puts "response is "
     puts response
     params = JSON.parse(response) unless response.nil?
 
     encrypted_message = decode64(params["ciphered_message"])
 
-    cipher = MatchThatCipher.new
+    @cipher = MatchThatCipher.new
     
-    cipher.setup_decipher(decrypted_message, decrypted_iv)
+    @cipher.setup_decipher(@cipher_key, @cipher_iv)
 
-    message = cipher.decrypt_with_cipher(encrypted_message)
+    message = @cipher.decrypt_with_cipher(encrypted_message)
     puts "message is "
     puts message
+    #@bank_client.close
+  end
 
-    bank_client_socket.close
+  def secure_connection
+    host = 'localhost'
+    port = 2000
+    @bank_client = TCPSocket.open(host, port)
+    request = { 'public_key' => encode64(@bank_crypto.public_key) }.to_json
+    @bank_client.puts(request)
+    response = @bank_client.gets
+    params = JSON.parse(response) unless response.nil?
+    puts "params is "
+    puts params
+    #unless not params.nil?
+    puts 'in block'
+    encrypted_message = params["encrypted_cipher"]
+    encrypted_message = decode64(encrypted_message)
+
+    encrypted_iv = params["encrypted_cipher_iv"]
+
+    decrypted_message = @bank_crypto.decrypt_message_with_private_key(encrypted_message)
+    @cipher_key = decrypted_message
+    decrypted_iv = @bank_crypto.decrypt_message_with_private_key(decode64(encrypted_iv))
+    @cipher_iv = decrypted_iv
+
+    puts "\n\n"
+    puts "decrypted mesage IS "
+    puts decrypted_message
+
+      #here is where we need to get the public key and encrypt it with the decrypted_message
+    #end
+    response = @bank_client.gets
+    params = JSON.parse(response) unless response.nil?
+    puts "params is "
+    puts params
+    server_public_key = decode64(params["public_key"])
+    encrypted_secret = @bank_crypto.encrypt_message_with_recipient_public_key(server_public_key, decrypted_message)
+    @bank_client.puts( {"encrypted_secret" => encode64(encrypted_secret) }.to_json )
+    puts "sent encrypted secret"
+    return true
+
+
   end
 end
 
 mcb = MintClientBank.new
-mcb.run
+if mcb.secure_connection
+  puts "calling run"
+  mcb.run
+end
 
