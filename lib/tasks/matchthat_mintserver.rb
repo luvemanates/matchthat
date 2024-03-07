c2e62e5c44a1eaf3ef9459745f618a0b9eca0aac
luvemanates <luvemanates@gmail.com>
2024-03-07 12:54:23 -0800

    Sepearted the secure connection portion into its own method - works the same as before (#8)

--- lib/tasks/matchthat_mintserver.rb
+++ lib/tasks/matchthat_mintserver.rb
@@ -20,71 +20,34 @@ end
 
 class MintServer
   
+  attr_accessor :server
+  attr_accessor :client
+  attr_accessor :cypto
+  attr_accessor :mint
+  attr_accessor :mint_wallet
+  attr_accessor :exchange
+  attr_accessor :cipher
 
   def initialize
+    @mint = MatchMintingBank.new 
+    @crypto = MatchThatCryptography.new(MatchThatCryptography::CONFIG)
+    @mint_wallet = DigitalWallet.new('mint_wallet', @crypto)
+    @exchange = CentralizedExchange.new
   end
 
 
   def run
-    host = 'localhost'
-    port = 2000
-    server = TCPServer.open(host, port) # Bind to port 2000
-    mint = MatchMintingBank.new 
-    matchthatmint_crypto = MatchThatCryptography.new(MatchThatCryptography::CONFIG)
-    #matchthatbank_crypto = MatchThatCryptography.new(MatchThatCryptography::CONFIG)
-    mint_wallet = DigitalWallet.new('mint_wallet', matchthatmint_crypto)
-    exchange = CentralizedExchange.new
+      #coin = mint.mint(1)
+      #@exchange.coins << coin
+      #@mint_wallet.debit_coin(coin)
 
-    matchthat_cipher = MatchThatCipher.new
-    matchthat_cipher.setup_cipher()
-
-    random_secret = matchthat_cipher.cipher_key #(0...16).map { (65 + rand(26)).chr }.join
-    puts "random secret is " + random_secret.to_s
-
-    loop {
-      client = server.accept
-      params = JSON.parse(client.gets)
-      if not params["public_key"].nil?
-        puts "the public key from the client is "
-        puts params["public_key"]
-        client_public_key = decode64( params["public_key"] )
-        #other_secret = matchthat_crypto.encrypt_message(mint_wallet_crypto_card, params[:public_key], "Requesting deposit authorization:", random_secret)
-        encrypted_message = matchthatmint_crypto.encrypt_message_with_recipient_public_key(client_public_key, random_secret)
-    #    decrypted_message = matchthatbank_crypto.decrypt_message_with_private_key(encrypted_message)
-        puts 'encrypted message ' 
-        puts 'encoding is ' 
-        puts encrypted_message.encoding
-    #    encrypted_message = "See if this is even recieved"
-        #puts encrypted_message
-        encrypted_message_utf8 = encode64(encrypted_message)
-        puts "sending message with encoding " 
-        puts encrypted_message_utf8.encoding
-        encrypted_cipher_iv = matchthatmint_crypto.encrypt_message_with_recipient_public_key(client_public_key, matchthat_cipher.cipher_iv)
-        client.puts({'encrypted_cipher' => encrypted_message_utf8, "encrypted_cipher_iv" => encode64(encrypted_cipher_iv)}.to_json)
-
-        #params = JSON.parse(client.gets)
-        #puts "returning from client after decrypt"
-        #puts params
-        client.puts({'public_key' => encode64(matchthatmint_crypto.public_key.to_s)}.to_json)
-      end
-      params = JSON.parse(client.gets)
-      puts "params parsed"
-      puts params
-      client_response = decode64(params["encrypted_secret"])
-      decrypted_message = matchthatmint_crypto.decrypt_message_with_private_key(client_response)
-      puts decrypted_message
-      puts "VERIFIED" if decrypted_message == random_secret
-
-      #client.puts params[:public_key ]
-      #raise params.inspect
+      #CentralizedExchange.transfer( @mint_wallet, bank_wallet, 1)
       data = "I sent this message - later it should turn into a coin"
-      ciphered_message = matchthat_cipher.encrypt_with_cipher(data) 
+      ciphered_message = @cipher.encrypt_with_cipher(data) 
       ciphered_message = encode64(ciphered_message)
-      client.puts({'ciphered_message' => ciphered_message}.to_json)
-    }
+      @client.puts({'ciphered_message' => ciphered_message}.to_json)
 
-    client.close
-
+      loop do
 =begin
 loop do
   client = server.accept    # Wait for a client to connect
@@ -99,9 +62,65 @@ loop do
   client.close
 end
 =end
+    end
+    @client.close
+  end
+
+  def secure_connection
+    host = 'localhost'
+    port = 2000
+    @server = TCPServer.open(host, port) # Bind to port 2000
+    #matchthatbank_crypto = MatchThatCryptography.new(MatchThatCryptography::CONFIG)
+
+    @cipher = MatchThatCipher.new
+    @cipher.setup_cipher()
+
+    random_secret = @cipher.cipher_key #(0...16).map { (65 + rand(26)).chr }.join
+    puts "random secret is " + random_secret.to_s
+
+    @client = @server.accept
+    params = JSON.parse(client.gets)
+    if not params["public_key"].nil?
+      puts "the public key from the client is "
+      puts params["public_key"]
+      client_public_key = decode64( params["public_key"] )
+      #other_secret = matchthat_crypto.encrypt_message(mint_wallet_crypto_card, params[:public_key], "Requesting deposit authorization:", random_secret)
+      encrypted_message = @crypto.encrypt_message_with_recipient_public_key(client_public_key, random_secret)
+  #    decrypted_message = matchthatbank_crypto.decrypt_message_with_private_key(encrypted_message)
+      puts 'encrypted message ' 
+      puts 'encoding is ' 
+      puts encrypted_message.encoding
+  #    encrypted_message = "See if this is even recieved"
+      #puts encrypted_message
+      encrypted_message_utf8 = encode64(encrypted_message)
+      puts "sending message with encoding " 
+      puts encrypted_message_utf8.encoding
+      encrypted_cipher_iv = @crypto.encrypt_message_with_recipient_public_key(client_public_key, @cipher.cipher_iv)
+      @client.puts({'encrypted_cipher' => encrypted_message_utf8, "encrypted_cipher_iv" => encode64(encrypted_cipher_iv)}.to_json)
+
+      #params = JSON.parse(client.gets)
+      #puts "returning from client after decrypt"
+      #puts params
+      @client.puts({'public_key' => encode64(@crypto.public_key.to_s)}.to_json)
+    end
+    params = JSON.parse(@client.gets)
+    puts "params parsed"
+    puts params
+    client_response = decode64(params["encrypted_secret"])
+    decrypted_message = @crypto.decrypt_message_with_private_key(client_response)
+    puts decrypted_message
+    puts "VERIFIED" if decrypted_message == random_secret
+    return true
+    #client.puts params[:public_key ]
+    #raise params.inspect
+
+
   end
 end
 
 ms = MintServer.new
-ms.run
+secure_connection = ms.secure_connection
+if secure_connection
+  ms.run
+end
 
